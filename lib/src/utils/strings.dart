4831b655c563a93b49875f488f9f42a30cc5e82d
jordyhers <jordy925@gmail.com>
2023-04-18 22:55:37 +0200

    created custom string value

--- lib/src/utils/strings.dart
+++ lib/src/utils/strings.dart
@@ -23,4 +23,7 @@ class Str {
     Str.myRoleText,
     'Yale Gala University',
   );
+
+  static String resumeHeader =
+      'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
 }
