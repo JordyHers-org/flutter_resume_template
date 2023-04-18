222b4ff3fb32868513a33d66b079de67916f13c6
jordyhers <jordy925@gmail.com>
2023-04-18 22:55:57 +0200

    added color parser builder.

--- lib/src/styles/theme.dart
+++ lib/src/styles/theme.dart
@@ -13,6 +13,32 @@ enum TemplateType {
   none,
 }
 
+class CustomColors {
+  static MaterialColor primaryColorModern =
+      buildMaterialColor(const Color(0xFF323b4c));
+
+  // This Function creates a material color from HEX color value
+  static MaterialColor buildMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    Map<int, Color> swatch = {};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    for (var strength in strengths) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    }
+    return MaterialColor(color.value, swatch);
+  }
+}
+
 class TemplateTheme {
   /// templateType
   final TemplateType type;
@@ -30,6 +56,12 @@ class TemplateTheme {
   TemplateTheme._internal(this.type, this.themeData);
 
   static TextTheme globalTextTheme = TextTheme(
+    headlineLarge: const TextStyle(
+      letterSpacing: 2.0,
+      fontWeight: FontWeight.w800,
+      fontSize: 38,
+      color: Colors.black,
+    ),
     bodySmall: TextStyle(
         fontWeight: FontWeight.normal,
         fontSize: 17,
@@ -61,8 +93,8 @@ class TemplateTheme {
   static TemplateTheme modern = TemplateTheme(
     TemplateType.modernResume,
     ThemeData(
-      shadowColor: Colors.teal,
-      primarySwatch: Colors.teal,
+      shadowColor: CustomColors.primaryColorModern,
+      primarySwatch: CustomColors.primaryColorModern,
       fontFamily: GoogleFonts.bebasNeue().fontFamily,
       dividerColor: Colors.grey.shade300,
       textTheme: globalTextTheme,
