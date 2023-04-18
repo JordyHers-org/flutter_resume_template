a1f1d9329fc07d736cda938e736828f8ae29606b
jordyhers <jordy925@gmail.com>
2023-04-18 22:55:17 +0200

    updated main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -20,7 +20,7 @@ class _MyAppState extends State<MyApp> {
       home: SafeArea(
         child: Scaffold(
           body: FlutterResumeTemplate(
-            templateTheme: TemplateTheme.none,
+            templateTheme: TemplateTheme.business,
             data: data,
           ),
         ),
