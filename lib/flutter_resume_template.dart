eca03f981d5fae448bf5fc49e8af72af7ba47b27
jordyhers <jordy925@gmail.com>
2023-04-22 23:51:48 +0200

    exported files

--- lib/flutter_resume_template.dart
+++ lib/flutter_resume_template.dart
@@ -6,5 +6,7 @@ export 'package:flutter_resume_template/src/layout/layout_classic.dart';
 export 'package:flutter_resume_template/src/layout/layout_modern.dart';
 export 'package:flutter_resume_template/src/layout/layout_technical.dart';
 export 'package:flutter_resume_template/src/model/resume_template_data_model.dart';
+export 'package:flutter_resume_template/src/repository/pdf_saver.dart';
 export 'package:flutter_resume_template/src/styles/theme.dart';
 export 'package:flutter_resume_template/src/utils/config.dart';
+export 'package:flutter_resume_template/src/utils/enums.dart';
