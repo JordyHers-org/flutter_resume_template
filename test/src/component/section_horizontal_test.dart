e388600ca0ff19d991686f54e2a3a55fa4402178
jordyhers <jordy925@gmail.com>
2023-04-24 17:09:02 +0200

    updated tests

--- test/src/component/section_horizontal_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('This test will always pass', () {
-    // This test doesn't actually test anything, but it will always pass
-    expect(1 + 1, equals(2));
-  });
-}
