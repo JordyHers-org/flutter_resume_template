9054e5589436cb7889804d48e50021dd7917ce97
jordyhers <jordy925@gmail.com>
2023-04-24 09:25:42 +0200

    extracted AnimateButton

---
+++ lib/src/components/section_bottom_buttons.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_resume_template/flutter_resume_template.dart';
+
+class AnimateButton extends StatefulWidget {
+  const AnimateButton({
+    Key? key,
+    required this.onDragged,
+    required this.onSave,
+    required this.isDragged,
+  }) : super(key: key);
+
+  final VoidCallback onDragged;
+  final Function() onSave;
+  final bool isDragged;
+
+  @override
+  State<AnimateButton> createState() => _AnimateButtonState();
+}
+
+class _AnimateButtonState extends State<AnimateButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 20,
+      right: 20,
+      child: Row(children: [
+        OutlinedButton(
+          onPressed: () => widget.onDragged.call(),
+          style: OutlinedButton.styleFrom(
+              backgroundColor: widget.isDragged
+                  ? Colors.grey
+                  : Theme.of(context).primaryColor,
+              foregroundColor: Colors.white),
+          child: Text(
+            widget.isDragged ? 'Reset' : 'Animate',
+            style: Theme.of(context)
+                .textTheme
+                .titleSmall
+                ?.copyWith(color: Colors.white, fontSize: 12),
+          ),
+        ),
+        IconButton(
+          onPressed: widget.onSave,
+          icon: const Icon(Icons.download),
+        ),
+      ]),
+    );
+  }
+}
