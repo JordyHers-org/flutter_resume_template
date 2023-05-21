bf97bbf453595844ce53417b017a25913d8ed94a
jordyhers <jordy925@gmail.com>
2023-05-21 22:03:54 +0200

    #31 test: improve test

--- test/src/component/section_rating_test.dart
+++ test/src/component/section_rating_test.dart
@@ -8,8 +8,11 @@ void main() {
   testWidgets('RatingWidget displays correct number of filled stars',
       (WidgetTester tester) async {
     // Build the RatingWidget with a rating of 3
-    await tester.pumpWidget(await Launch.pumpWidget(
-        const RatingWidget(rating: 3, title: 'English')));
+    await tester.pumpWidget(await Launch.pumpWidget(RatingWidget(
+      rating: 3,
+      title: 'English',
+      autoplay: true,
+    )));
 
     // Verify that the RatingWidget contains 3 filled stars and 2 empty stars
     expect(find.byIcon(Icons.star), findsNWidgets(3));
